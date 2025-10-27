504cfa6610d58b99563fb231c36ab6cb0e275e3d
ledok <supermaster.ua.lviv@gmail.com>
2025-10-27 10:43:56 +0100

    Some armor materials

--- src/main/java/net/ledok/registry/ArmorMaterialRegistry.java
+++ src/main/java/net/ledok/registry/ArmorMaterialRegistry.java
@@ -1,6 +1,62 @@
 package net.ledok.registry;
 
+import net.minecraft.Util;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class ArmorMaterialRegistry {
+
+    public static final Holder<ArmorMaterial> PRIEST_T6 = register("priest_t6", Util.make(new EnumMap(ArmorItem.Type.class), enumMap -> {
+        enumMap.put(ArmorItem.Type.BOOTS, 3);
+        enumMap.put(ArmorItem.Type.LEGGINGS, 6);
+        enumMap.put(ArmorItem.Type.CHESTPLATE, 8);
+        enumMap.put(ArmorItem.Type.HELMET, 3);
+        enumMap.put(ArmorItem.Type.BODY, 11);
+    }), 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.12F, () -> Ingredient.of(Items.NETHERITE_INGOT));
+
+    public static final Holder<ArmorMaterial> FROST_T6 = register("frost_t6", Util.make(new EnumMap(ArmorItem.Type.class), enumMap -> {
+        enumMap.put(ArmorItem.Type.BOOTS, 3);
+        enumMap.put(ArmorItem.Type.LEGGINGS, 6);
+        enumMap.put(ArmorItem.Type.CHESTPLATE, 8);
+        enumMap.put(ArmorItem.Type.HELMET, 3);
+        enumMap.put(ArmorItem.Type.BODY, 11);
+    }), 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 2.0F, 0.12F, () -> Ingredient.of(Items.NETHERITE_INGOT));
+
+    private static Holder<ArmorMaterial> register(
+            String string, EnumMap<ArmorItem.Type, Integer> enumMap, int i, Holder<SoundEvent> holder, float f, float g, Supplier<Ingredient> supplier
+    ) {
+        List<ArmorMaterial.Layer> list = List.of(new ArmorMaterial.Layer(ResourceLocation.withDefaultNamespace(string)));
+        return register(string, enumMap, i, holder, f, g, supplier, list);
+    }
+
+    private static Holder<ArmorMaterial> register(
+            String string,
+            EnumMap<ArmorItem.Type, Integer> enumMap,
+            int i,
+            Holder<SoundEvent> holder,
+            float f,
+            float g,
+            Supplier<Ingredient> supplier,
+            List<ArmorMaterial.Layer> list
+    ) {
+        EnumMap<ArmorItem.Type, Integer> enumMap2 = new EnumMap(ArmorItem.Type.class);
+
+        return Registry.registerForHolder(
+                BuiltInRegistries.ARMOR_MATERIAL, ResourceLocation.withDefaultNamespace(string), new ArmorMaterial(enumMap2, i, holder, supplier, list, f, g)
+        );
+    }
 
     public static void initialize() {
     }
--- src/main/java/net/ledok/registry/ArmorRegistry.java
+++ src/main/java/net/ledok/registry/ArmorRegistry.java
@@ -4,7 +4,6 @@ import net.ledok.Init.ItemInit;
 import net.ledok.Items.FrostT6ArmorItem;
 import net.ledok.Items.PriestT6ArmorItem;
 import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Supplier;
@@ -13,37 +12,37 @@ public class ArmorRegistry {
 
     // PRIEST_ARMOR_T6
     public static final Item PRIEST_ARMOR_HELMET = ItemInit.register(
-            new PriestT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)),
+            new PriestT6ArmorItem(ArmorMaterialRegistry.PRIEST_T6, ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)),
             "t6_priest_helmet"
     );
     public static final Item PRIEST_ARMOR_CHESTPLATE = ItemInit.register(
-            new PriestT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)),
+            new PriestT6ArmorItem(ArmorMaterialRegistry.PRIEST_T6, ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)),
             "t6_priest_chestplate"
     );
     public static final Item PRIEST_ARMOR_LEGGINGS = ItemInit.register(
-            new PriestT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)),
+            new PriestT6ArmorItem(ArmorMaterialRegistry.PRIEST_T6, ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)),
             "t6_priest_leggings"
     );
     public static final Item PRIEST_ARMOR_BOOTS = ItemInit.register(
-            new PriestT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)),
+            new PriestT6ArmorItem(ArmorMaterialRegistry.PRIEST_T6, ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)),
             "t6_priest_boots"
     );
 
     // FROST_ARMOR_T6
     public static final Item FROST_ARMOR_HELMET = ItemInit.register(
-            new FrostT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)),
+            new FrostT6ArmorItem(ArmorMaterialRegistry.FROST_T6, ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)),
             "t6_frost_helmet"
     );
     public static final Item FROST_ARMOR_CHESTPLATE = ItemInit.register(
-            new FrostT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)),
+            new FrostT6ArmorItem(ArmorMaterialRegistry.FROST_T6, ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)),
             "t6_frost_chestplate"
     );
     public static final Item FROST_ARMOR_LEGGINGS = ItemInit.register(
-            new FrostT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)),
+            new FrostT6ArmorItem(ArmorMaterialRegistry.FROST_T6, ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)),
             "t6_frost_leggings"
     );
     public static final Item FROST_ARMOR_BOOTS = ItemInit.register(
-            new FrostT6ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)),
+            new FrostT6ArmorItem(ArmorMaterialRegistry.FROST_T6, ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)),
             "t6_frost_boots"
     );
 
