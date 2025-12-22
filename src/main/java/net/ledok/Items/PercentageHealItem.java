71e1de02efb3b458a5da2a20ebd68471df4f4561
ledok <supermaster.ua.lviv@gmail.com>
2025-12-22 12:52:16 +0100

    More textures Ver 7.4.4

--- src/main/java/net/ledok/Items/PercentageHealItem.java
+++ src/main/java/net/ledok/Items/PercentageHealItem.java
@@ -8,7 +8,6 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import java.util.List;
 
